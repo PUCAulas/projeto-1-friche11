d8d2ada07d6292fd70bb32c43b6a94cf1d299729
Victor Friche <vicfriche@gmail.com>
2023-09-14 18:38:21 -0300

    Teste da classe equipamento

--- codigo/test/EquipamentoTest.java
+++ codigo/test/EquipamentoTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import business.Equipamento;
+
 class EquipamentoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void testEquipamento() {
+        Equipamento equipamento = new Equipamento("E001", "Escavadeira", 100.0);
+        assertEquals("E001", equipamento.getCodigo());
+        assertEquals("Escavadeira", equipamento.getDescricao());
+        assertEquals(100.0, equipamento.getValorDiario(), 0.01);
+    }
 
 }
