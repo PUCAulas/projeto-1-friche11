a512b92ac8a4864dc36d052b86aae78b0f2832a0
VitorDeF <vitorfigp@gmail.com>
2023-09-17 19:35:41 -0300

    teste aluguel

--- codigo/test/AluguelTest.java
+++ codigo/test/AluguelTest.java
@@ -8,36 +8,33 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import app.Cliente;
+import business.Cliente;
 import business.Aluguel;
-import business.Empresa;
+import business.Equipamento;
 
 class AluguelTest {
 	
 	Aluguel a1;
 	LocalDate i, t;
 	Cliente c;
-	Empresa e;
+	Equipamento e;
 	
 	@BeforeEach
 	void setUp() {
-		e = new Empresa();
+		e = new Equipamento(1, "Descrição", 10);
 		c = new Cliente("Vitor", "0");
 		a1 = new Aluguel(c, e, i, t);
-	}
-	
-	@Before
-	void setBefore() {
-		i.plusDays(10);
-		t.toString();
-		
+		i = LocalDate.now();
+		t = LocalDate.now().plusDays(10);
 	}
 	
 	@Test
 	void aluguelTest() {
 		assertEquals(a1.getCliente(), c);
-		assertEquals(e, null);
-		assertEquals(i, null);
+		assertEquals(e.getDescricao(), "Descrição");
+		assertEquals(i.getDayOfMonth(), 17);
+		assertEquals(i.getMonthValue(), 9);
+		assertEquals(i.getYear(), 2023);
 		
 	}
 
