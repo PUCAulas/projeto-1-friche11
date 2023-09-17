7226ffd332999c026bf07c4e782229126a0394a1
VitorDeF <vitorfigueiredopereira@gmail.com>
2023-09-17 16:14:41 -0300

    Att Aluguel

--- codigo/business/Aluguel.java
+++ codigo/business/Aluguel.java
@@ -1,5 +1,51 @@
 package business;
+import java.time.LocalTime;
 
 public class Aluguel {
+	private Cliente cliente;
+	private Equipamento equipamento;
+	private LocalTime inicio;
+	private LocalTime termino;
+	private int valor;
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+	public void setEquipamento(Equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
+	public LocalTime getInicio() {
+		return inicio;
+	}
+	public void setInicio(LocalTime inicio) {
+		this.inicio = inicio;
+	}
+	public LocalTime getTermino() {
+		return termino;
+	}
+	public void setTermino(LocalTime termino) {
+		this.termino = termino;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public Aluguel(Cliente cliente, Equipamento equipamento, LocalTime inicio, LocalTime termino) {
+		this.cliente = cliente;
+		this.equipamento = equipamento;
+		this.inicio = inicio;
+		this.termino = termino;
+	}
+	
+	
 
 }
