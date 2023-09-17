c0e33fc78dff4655afcc1409e2433f007cd9954d
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-09-17 15:33:52 -0300

    Criando Testes Da Classe Cliente

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -6,9 +6,35 @@ import org.junit.jupiter.api.Test;
 
 class ClienteTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	   private Cliente cliente;
 
+	    @BeforeEach
+	    public void setUp() {
+	        // Configuração inicial para cada teste
+	        cliente = new Cliente("João da Silva", "123.456.789-00");
+	    }
+
+	    @Test
+	    public void testGetNome() {
+	        assertEquals("João da Silva", cliente.getNome());
+	    }
+
+	    @Test
+	    public void testGetCpf() {
+	        assertEquals("123.456.789-00", cliente.getCpf());
+	    }
+
+	    @Test
+	    public void testConstrutor() {
+	        assertEquals("João da Silva", cliente.getNome());
+	        assertEquals("123.456.789-00", cliente.getCpf());
+	    }
+
+	    @Test
+	    public void testAlterarNome() {
+	        cliente = new Cliente("Maria Souza", "987.654.321-00");
+	        assertEquals("Maria Souza", cliente.getNome());
+	        assertEquals("987.654.321-00", cliente.getCpf());
+	    }
+	
 }
