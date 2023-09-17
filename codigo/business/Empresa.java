b428f7b144e0bd3e63c482d470c85d54fd2f4083
Victor Friche <vicfriche@gmail.com>
2023-09-17 17:32:30 -0300

    Metodo gerarRelatorioMensal

--- codigo/business/Empresa.java
+++ codigo/business/Empresa.java
@@ -22,6 +22,31 @@ public class Empresa {
 	     long diasAlugados = aluguel.getInicio().until(aluguel.getTermino()).getDays();
 	     return diasAlugados * aluguel.getEquipamento().getValorDiario();
 	    }
+	 
+	 public double gerarRelatorioMensal(LocalDate data) {
+		    double faturamentoTotal = 0.0;
+		    int mesDesejado = data.getMonthValue();
+
+		    // Verificar aluguéis atuais
+		    for (int i = 0; i < alugueisAtuais.size(); i++) {
+		        Aluguel aluguel = alugueisAtuais.get(i);
+		        LocalDate dataAluguel = aluguel.getInicio();
+		        if (dataAluguel.getMonthValue() == mesDesejado) {
+		            faturamentoTotal += calcularValorTotalAluguel(aluguel);
+		        }
+		    }
+
+		    // Verificar aluguéis passados
+		    for (int i = 0; i < alugueisPassados.size(); i++) {
+		        Aluguel aluguel = alugueisPassados.get(i);
+		        LocalDate dataAluguel = aluguel.getInicio();
+		        if (dataAluguel.getMonthValue() == mesDesejado) {
+		            faturamentoTotal += calcularValorTotalAluguel(aluguel);
+		        }
+		    }
+
+		    return faturamentoTotal;
+		}
 
 	public void registrarAluguel(Cliente cliente1, Equipamento equipamento1, LocalDate dataInicio1,
 			LocalDate dataFim1) {
