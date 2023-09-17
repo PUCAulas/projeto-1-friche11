1e66a214cb94f94654de0cfd9cb6473200e6c9e6
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-09-17 15:36:29 -0300

    Refatorando os Testes para Classe Cliente

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -2,7 +2,10 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import app.Cliente;
 
 class ClienteTest {
 
